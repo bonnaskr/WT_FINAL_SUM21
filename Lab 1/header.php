d1ead1ad7a209ee99e6aadcec089d0cda79d16d2
bonnaskr <60335158+bonnaskr@users.noreply.github.com>
2021-08-15 01:13:03 +0600

    done

---
+++ Lab 1/header.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+        <fieldset>
+    <a href="dashboard.php">
+        <input type="button" value="Home">
+    </a>
+    <a href="add_student.php">
+        <input type="button" value="ADD Student">
+    </a>
+    <a href="delete_student.php">
+        <input type="button" value="Delete Student">
+    </a>
+    <a href="all_student.php">
+        <input type="button" value="All Student">
+    </a>
+    <a href="#">
+        <input type="button" value="All Dept">
+    </a>
+    <a href="#">
+        <input type="button" value="Add Dept">
+    </a>
+    </fieldset>
+    </body>
+</html>
