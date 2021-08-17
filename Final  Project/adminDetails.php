f7efd9aa228b47648b1b112cd75a378968854167
bonnaskr <60335158+bonnaskr@users.noreply.github.com>
2021-08-17 16:22:31 +0600

    done

---
+++ Final  Project/adminDetails.php
@@ -0,0 +1,69 @@
+<?php
+
+    require_once 'controllers/adminController.php';
+    require_once 'controllers/crudPubController.php';
+
+
+
+
+   
+		
+    $public = getAllPublic();
+
+    
+
+
+
+?>
+
+<html>
+    <head>
+
+
+    </head>
+
+
+
+    <body>
+        
+
+
+
+        <div id="demo"></div> 
+
+
+
+    <ul>
+
+     <li><a class="btn-link" href="updatelatestnewsonCovid.php">=>Latest Covid News</a> <br> <br></li>
+     <li><a class="btn-link" href="AllPublicDetails.php">=>All Public Details </a> <br> <br></li>
+     <li><a class="btn-link" href="createPublicinfo.php">=>Create Public</a> <br> <br></li>
+
+     <li><a class="btn-link" href="allregPublic.php">=>All Registered Public</a> <br> <br></li>
+
+     <li><a class="btn-link" href="admin1.php">=>Create Admin</a> <br> <br></li>
+
+
+    </ul>
+
+
+ 
+
+
+
+
+
+     
+
+
+
+        
+
+
+
+
+
+    </body>
+</html>   
+    
+    
